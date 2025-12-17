a223db4f2c1b78773f2759b7eb8820b05bf6f333
Lucas Pereira <lucasp28.contato@gmail.com>
2025-12-17 17:05:04 -0300

    fix callback route config param

--- config/nfse.php
+++ config/nfse.php
@@ -10,5 +10,5 @@ return [
     'environment' => env('NFSE_ENVIRONMENT', env('APP_ENV') === 'local' ? 'developer' : 'production'),
     'log_channel' => env('NFSE_LOG_CHANNEL', 'nfse'),
     'dolar_fallback_value' => 5.3,
-    'nfse.callback_route' => env('NFSE_CALLBACK_URL')
+    'callback_route' => env('NFSE_CALLBACK_URL')
 ];
